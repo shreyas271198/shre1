35b7366848b8f58b27968f81bef21ca6cd363446
Shreyas Chaudhari <49345284+shreyas271198@users.noreply.github.com>
2025-04-13 19:54:56 +0530

    Update javascript.js for old images

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ function loadImages() {
     const gallery = document.querySelector('.gallery');
     const imageFolder = 'bigimg/';
     const thumbFolder = 'images/';
-    const imageNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,19,20,21,22,23,24,25,26]; // Array of image numbers
+    const imageNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,19,28,20,27,29,30,31,33,32,21,22,23,24,25,26]; // Array of image numbers
 
     imageNumbers.forEach(number => {
         const circle = document.createElement('div');
